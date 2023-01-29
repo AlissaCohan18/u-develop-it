526a816d96ba2a28a0b571b41b765a1f5132c6a5
Alissa Cohan <alissacohan18@gmail.com>
2023-01-29 13:36:52 -0500

    added voter table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 -- Drop candidates table first b/c foreign key constraint req parties table to exist
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -19,4 +20,13 @@ CREATE TABLE candidates (
     -- table and field it references ("id" field in the "parties" table)
     -- b/c contstraint relies on the parties table, parties MUST be defined first
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  -- if/when not entered manually, the default will be to use the CURRENT_TIME
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
